927e9c6d5c7431c40c5fa51815b9918783a8f74e
AdamZWinter <adam@bootstrapmusic.com>
2023-10-04 11:07:35 -0700

    minor edits

--- src/main/java/edu/greenriver/sdev/exampleprogram/db/IWeatherReadingRepository.java
+++ src/main/java/edu/greenriver/sdev/exampleprogram/db/IWeatherReadingRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 /**
- * This is a data layer that allows us to access WeathReading records
+ * This is a data layer that allows us to access WeatherReading records
  * in our database.
  */
 @Repository
